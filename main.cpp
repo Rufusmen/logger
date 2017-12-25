6ebb78f3f98edbc75e2f82a03893780aa63d5101
Jakub Mendyk <jakubmendyk.szkola+git@gmail.com>
2017-12-25 19:21:11 +0100

    Modify Logger implementation
    All operations on Logger happend through static methods
    such as init, debug, info...
    
    Logger must be initialized before usage using Logger::init,
    otherwise assertion during any of logging operation will
    fail.
    
    Added new log level - debug.
    
    Moved time to Logger::formatted_time to avoid pollution of
    global scope.
    
    Change hour filed in logs from 12H to 24H.
    
    Disabled buffering on logFile stream.
    
    Refactored common functionalities of debug, info, warn and err
    to logEntry.
    
    Removed _vsnprintf and inlined it's functionality inside logEntry.
    
    I am very unhappy with the fact that naked Loggger* pointer must
    be initialized in logger.cpp but there seems to be not good solution
    in the current situatation.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
 #include "logger.h"
 
 int main() {
-    Logger("nowylog.txt");
-    LOGGER->info("Chyba działa");
+    Logger::init("nowylog.txt");
+    Logger::debug("Jakiś debug");
+    Logger::info("Chyba działa");
     int x=5,y=x--;
-    LOGGER->err("x jest: %d , a miał byc %d",y,x);
-    LOGGER->warn("innyy stuff : %c %f %0.1lf",'a',0.5123123,847387.312312);
+    Logger::init("nowylog.txt");
+    Logger::warn("innyy stuff : %c %f %0.1lf", 'a', 0.5123123, 847387.312312);
+    Logger::err("x jest: %d , a miał byc %d", y, x);
     return 0;
 }
